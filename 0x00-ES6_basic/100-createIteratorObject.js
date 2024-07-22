51463b6ae3414359388e665671bb3bceeb6ccb65
Mwan3kii <agathawanja303@gmail.com>
2024-07-22 23:44:06 +0300

    Filter unwanted properties

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,9 @@ export default function createIteratorObject(report) {
   const { allEmployees } = report;
   const employeesArray = [];
   for (const department in allEmployees) {
-    employeesArray.push(...allEmployees[department]);
+    if (Object.prototype.hasOwnProperty.call(allEmployees, department)) {
+      employeesArray.push(...allEmployees[department]);
+    }
   }
   return employeesArray[Symbol.iterator]();
 }
