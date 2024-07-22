a2a01dcb041e48cc24ccf71ce9c41d432115444a
Mwan3kii <agathawanja303@gmail.com>
2024-07-22 23:48:50 +0300

    Use for of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const idx in array) {
-    if (Object.prototype.hasOwnProperty.call(array, idx)) {
-      newArray[idx] = appendString + array[idx];
-    }
+  for (const idx of array) {
+    newArray.push(`${appendString}${idx}`);
   }
   return newArray;
 }
