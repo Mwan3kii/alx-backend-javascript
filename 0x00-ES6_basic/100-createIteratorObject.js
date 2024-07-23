7c7dc52f7be5d651ca765f6118637946c265c394
Mwan3kii <agathawanja303@gmail.com>
2024-07-23 18:39:44 +0300

    Use Object.values instead

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,7 @@
 export default function createIteratorObject(report) {
-  const { allEmployees } = report;
-  const employeesArray = [];
-  for (const department in allEmployees) {
-    if (Object.prototype.hasOwnProperty.call(allEmployees, department)) {
-      employeesArray.push(...allEmployees[department]);
-    }
+  let employeeArray = [];
+  for (const value of Object.values(report.allEmployees)) {
+    employeeArray = [...employeeArray, ...value];
   }
-  return employeesArray[Symbol.iterator]();
+  return employeeArray;
 }
