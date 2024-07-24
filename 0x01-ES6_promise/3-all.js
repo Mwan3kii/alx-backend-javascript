594ef5e0c230d3ccdbf36f407e6b382bdb8c3537
Mwan3kii <agathawanja303@gmail.com>
2024-07-25 00:27:48 +0300

    Fix bugs

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([userResponse]) => {
-      console.log(`${userResponse.firstName} ${userResponse.lastName}`);
+  return uploadPhoto()
+    .then(({ body }) => {
+      createUser()
+        .then(({ firstName, lastName }) => {
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
