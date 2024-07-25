0262aaebd663e12370fca6d2f407149d2f56c226
Mwan3kii <agathawanja303@gmail.com>
2024-07-25 19:03:50 +0300

    Class extends Building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent class constructor
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
