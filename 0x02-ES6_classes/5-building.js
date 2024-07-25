c029bf8ce4ca2a417204eb658bacb3b9e5999804
Mwan3kii <agathawanja303@gmail.com>
2024-07-25 19:52:38 +0300

    Fix eslint issues

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-      if (new.target === Building) {
-        throw new Error('Cannot instantiate abstract class Building directly');
-      }
-      this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
