6a30b980a667a2908f22cd287bc26b2776bc4021
Mwan3kii <agathawanja303@gmail.com>
2024-07-25 20:30:11 +0300

    Fix bugs

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,19 +1,12 @@
-import Car from './10-car.js';
-
-const _range = Symbol('range');
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color); // Call the parent class constructor
-    this[_range] = range;
+    super(brand, motor, color);
+    this._range = range;
   }
 
-  get range() {
-    return this[_range];
-  }
-
-  cloneCar() {
-    // Return an instance of the Car class instead of EVCar
-    return new Car(this[_brand], this[_motor], this[_color]);
+  static get [Symbol.species]() {
+    return Car;
   }
 }
