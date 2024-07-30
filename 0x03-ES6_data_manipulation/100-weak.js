ec0fd5ff736f9a64bd820b5d4201bcccb597fe2a
Mwan3kii <agathawanja303@gmail.com>
2024-07-30 18:11:25 +0300

    Fix eslint errors

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,13 +4,10 @@ export function queryAPI(endpoint) {
   if (!(endpoint && typeof endpoint === 'object' && 'protocol' in endpoint && 'name' in endpoint)) {
     throw new Error('Invalid endpoint');
   }
-
   let count = weakMap.get(endpoint) || 0;
   count += 1;
-
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-
   weakMap.set(endpoint, count);
 }
