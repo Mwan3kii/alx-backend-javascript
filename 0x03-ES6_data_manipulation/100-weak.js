73c01a5df25e9390bc20a5a44524b37320f08722
Mwan3kii <agathawanja303@gmail.com>
2024-07-30 17:57:15 +0300

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!(endpoint && typeof endpoint === 'object' && 'protocol' in endpoint && 'name' in endpoint)) {
+    throw new Error('Invalid endpoint');
+  }
+
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, count);
+}
