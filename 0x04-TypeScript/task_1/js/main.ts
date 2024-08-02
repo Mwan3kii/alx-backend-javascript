dd83287dda5d2af7dedeb8f534546a74a318f4ee
Mwan3kii <agathawanja303@gmail.com>
2024-08-02 20:41:34 +0300

    Add director interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
 export interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    location: string;
-    yearsOfExperience?: number;
-    [key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [key: string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
 }
