d78ad68af6cd9c558b42249952bb1661787605fa
Mwan3kii <agathawanja303@gmail.com>
2024-08-02 20:52:10 +0300

    Add student class and methods

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,36 @@ export interface Teacher {
 export interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+export interface printTeacher {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacher = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+export interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class Student implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+  
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return 'Currently working';
+}
+  displayName(): string {
+    return this.firstName;
+  }
+}
