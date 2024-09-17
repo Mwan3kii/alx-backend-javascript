4b9f8678886eb528a5173d11e463ee993540e085
Mwan3kii <agathawanja303@gmail.com>
2024-09-17 22:01:44 +0300

    Crate http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,24 +1,24 @@
 const http = require('http');
 const { readFile } = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const host = '127.0.0.1';
+const PORT = 1245;
 
 function countStudents(fileName) {
   const students = {};
   const fields = {};
   let length = 0;
   return new Promise((resolve, reject) => {
-    readFile(fileName, (err, data) => {
-      if (err) {
-        reject(err);
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(error);
       } else {
         let output = '';
         const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
+        for (let j = 0; j < lines.length; j += 1) {
+          if (lines[j]) {
             length += 1;
-            const field = lines[i].toString().split(',');
+            const field = lines[j].toString().split(',');
             if (Object.prototype.hasOwnProperty.call(students, field[3])) {
               students[field[3]].push(field[0]);
             } else {
@@ -64,7 +64,7 @@ const app = http.createServer((req, res) => {
   }
 });
 
-app.listen(port, hostname, () => {
+app.listen(PORT, host, () => {
 });
 
 module.exports = app;
