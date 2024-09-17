69c0b5fb51e0e984aa642576d16b68f1ef453fe3
Mwan3kii <agathawanja303@gmail.com>
2024-09-17 21:16:26 +0300

    Reading file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,12 @@
 // 2-read_file.js
-
 const fs = require('fs');
 
 function countStudents(path) {
   try {
-    // Read the file synchronously
     const data = fs.readFileSync(path, 'utf8');
-
-    // Split the data into lines and filter out empty ones
     const lines = data.split('\n').filter(line => line.trim() !== '');
-
-    // Remove the header (first line) if present
     const header = lines.shift();
-
-    // Create an object to store students by their field
     const studentsByField = {};
-
-    // Parse each line and categorize students by field
     lines.forEach(line => {
       const [firstName, , , field] = line.split(',');
 
@@ -27,11 +17,7 @@ function countStudents(path) {
         studentsByField[field].push(firstName);
       }
     });
-
-    // Log the total number of students
     console.log(`Number of students: ${lines.length}`);
-
-    // Log the number of students per field and their first names
     for (const field in studentsByField) {
       const students = studentsByField[field];
       console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
