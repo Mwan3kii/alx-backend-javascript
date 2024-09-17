006585da2ec366af5732b7c6614c10a699e07f66
Mwan3kii <agathawanja303@gmail.com>
2024-09-17 21:08:01 +0300

    Test ouput

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
 // Displays user input message on new line
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-// Listen for user input on stdin (standard input)
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`); // Using stdout.write to avoid automatic newline
-
-  // Close stdin after input is received
-  process.exit();
-});
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  } else {
+    process.stdin.on('data', (data) => {
+      process.stdout.write(`Your name is: ${data.toString()}`);
+      process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
