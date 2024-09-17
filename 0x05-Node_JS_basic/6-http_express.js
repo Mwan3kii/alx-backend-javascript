8e5e84a7776dbd04c6deda0d15112e15def8f04c
Mwan3kii <agathawanja303@gmail.com>
2024-09-17 22:07:53 +0300

    Create http server usign express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+});
+
+module.exports = app;
