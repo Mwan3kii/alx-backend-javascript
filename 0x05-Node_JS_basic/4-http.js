d4b8a04d2acde8c9f2521b61db971900df1ba314
Mwan3kii <agathawanja303@gmail.com>
2024-09-17 21:38:02 +0300

    Create small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,11 @@
-// 4-http.js
-
 const http = require('http');
 const host = '127.0.0.1';
-
 const PORT = 1245;
-
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
-
 app.listen(PORT, host, () => {});
 
 module.exports = app;
