576ed22e537df9df2de483c874c78ec081e5009f
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 20:06:53 +0300

    Test files

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,33 +1,20 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
-const { sendPaymentRequestToApi } = require('./4-payment');
-const { calculateNumber } = require('./utils');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', function() {
-  let stub;
-  let spy;
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-  beforeEach(function() {
-    // Stub the calculateNumber function to always return 10
-    stub = sinon.stub(calculateNumber, 'calculateNumber').returns(10);
-
-    // Spy on console.log to verify the message
-    spy = sinon.spy(console, 'log');
-  });
-
-  afterEach(function() {
-    // Restore the stub and the spy
-    stub.restore();
-    spy.restore();
-  });
-
-  it('should call calculateNumber with SUM and correct arguments', function() {
+    dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-  });
-
-  it('should log the correct message', function() {
-    sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
