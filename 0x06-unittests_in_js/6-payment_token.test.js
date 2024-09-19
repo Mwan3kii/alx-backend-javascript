021ff7a79d7cd2f536fc0e11d62e919636d3aa68
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 21:37:22 +0300

    Test files

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,26 +1,12 @@
 const { expect } = require('chai');
-const { getPaymentTokenFromAPI } = require('./6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', function() {
-  it('should return the expected data when success is true', function(done) {
-    getPaymentTokenFromAPI(true).then(response => {
-      try {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
-        done(); // Indicate that the test is complete
-      } catch (error) {
-        done(error); // Pass the error to done if the assertion fails
-      }
-    }).catch(done); // Handle any promise rejections
-  });
-
-  it('should return undefined when success is false', function(done) {
-    getPaymentTokenFromAPI(false).then(response => {
-      try {
-        expect(response).to.be.undefined; // Since nothing is resolved, we expect undefined
-        done(); // Indicate that the test is complete
-      } catch (error) {
-        done(error); // Pass the error to done if the assertion fails
-      }
-    }).catch(done); // Handle any promise rejections
+describe('getPaymentTokenFromAPI', () => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      });
   });
 });
