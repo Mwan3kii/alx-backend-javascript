eb0f967c5e05908d658be3df22d8376982b1d44a
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 21:47:10 +0300

    Test files

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,11 +1,7 @@
-function getPaymentTokenFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ data: 'Successful response from the API' });
-    } else {
-      resolve();
-    }
-  });
-}
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
 
 module.exports = getPaymentTokenFromAPI;
