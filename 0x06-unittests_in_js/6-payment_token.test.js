b2ed87b065955da11a250742e181081ffe46f5c2
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 21:48:24 +0300

    Test files

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,8 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((res) => {
-        expect(res).to.deep.equal({data: 'Successful response from the API'});
+      .then((response) => {
+        expect(response).to.deep.equal({data: 'Successful response from the API'});
         done();
       });
   });
