aaaeb13a46df27bb59b8785e4636b2fff0d924b1
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 21:36:05 +0300

    Test files

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -3,27 +3,27 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let bigBrother;
+  let spyD;
 
   beforeEach(() => {
-    if (!bigBrother) {
-      bigBrother = sinon.spy(console);
+    if (!spyD) {
+      spyD = sinon.spy(console);
     }
   });
 
   afterEach(() => {
-    bigBrother.log.resetHistory();
+    spyD.log.resetHistory();
   });
 
   it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(spyD.log.calledWith('The total is: 120')).to.be.true;
+    expect(spyD.log.calledOnce).to.be.true;
   });
 
   it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(spyD.log.calledWith('The total is: 20')).to.be.true;
+    expect(spyD.log.calledOnce).to.be.true;
   });
 });
