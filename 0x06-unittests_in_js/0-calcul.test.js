0be9053823b96a8d5b368952a3c5619f64d7ec4d
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 18:34:46 +0300

    Add test files and dependancies

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+  it('should return 4 when inputs are 1 and 3', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when inputs are 1 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when inputs are 1.2 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return the sum even with negative numbers', function() {
+    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
+  });
+
+  it('should return the sum when one number is 0', function() {
+    assert.strictEqual(calculateNumber(0, 4.5), 5);
+  });
+});
