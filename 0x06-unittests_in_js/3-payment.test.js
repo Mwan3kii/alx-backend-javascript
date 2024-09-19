f052e0e99f85e58b9673d332f1a61d8b32db84ba
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 19:55:51 +0300

    Test file

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,19 +5,10 @@ const { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', function () {
   it('should call Utils.calculateNumber with SUM, 100, and 20', function () {
-    // Create a spy for Utils.calculateNumber
     const spy = sinon.spy(Utils, 'calculateNumber');
-
-    // Call the function
     sendPaymentRequestToApi(100, 20);
-
-    // Check that Utils.calculateNumber was called once
     expect(spy.calledOnce).to.be.true;
-
-    // Check that it was called with the correct arguments
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-
-    // Restore the spy
     spy.restore();
   });
 });
