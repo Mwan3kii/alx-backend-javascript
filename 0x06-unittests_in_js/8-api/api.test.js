8bb6249d00d998ce5a3d29c366d75b847a4ad31e
Mwan3kii <agathawanja303@gmail.com>
2024-09-19 21:18:41 +0300

    Regex integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,43 +1,13 @@
 const request = require('request');
-const chai = require('chai');
-const expect = chai.expect;
-const app = require('./api');
-const port = 7865;
+const { expect } = require('chai');
 
-describe('Index page', () => {
-  before((done) => {
-    // Start the server before running tests
-    app.listen(port, () => {
-      console.log(`Test server running on port ${port}`);
-      done();
-    });
-  });
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  after((done) => {
-    // Close the server after tests
-    app.close(done);
-  });
-
-  it('should return status code 200', (done) => {
-    request(`http://localhost:${port}`, (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return correct message', (done) => {
-    request(`http://localhost:${port}`, (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('should log correct message when server is started', (done) => {
-    const spy = sinon.spy(console, 'log');
-    
-    request(`http://localhost:${port}`, (error, response, body) => {
-      expect(spy.calledWith(`API available on localhost port ${port}`)).to.be.true;
-      spy.restore();
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
