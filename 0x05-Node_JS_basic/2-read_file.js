fb6e81830e1e098d301029ca1a65cf198ee7784b
Mwan3kii <agathawanja303@gmail.com>
2024-09-20 17:54:20 +0300

    Update file with fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-    const header = lines.shift();
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    lines.shift();
     const students = {};
-    lines.forEach(line => {
+    lines.forEach((line) => {
       const [firstName, , , field] = line.split(',');
 
       if (field) {
@@ -18,8 +18,10 @@ function countStudents(path) {
     });
     console.log(`Number of students: ${lines.length}`);
     for (const field in students) {
-      const student = students[field];
-      console.log(`Number of students in ${field}: ${student.length}. List: ${student.join(', ')}`);
+      if (Object.prototype.hasOwnProperty.call(students, field)) {
+        const student = students[field];
+        console.log(`Number of students in ${field}: ${student.length}. List: ${student.join(', ')}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
